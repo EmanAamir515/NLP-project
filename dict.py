c4626f768d5dc4b8dcc51e660d8ff370e272c6fd
EmanAamir515 <eman712222@gmail.com>
2025-10-18 00:58:13 +0500

    Add dictionaries for Joy and Sadness words

--- dict.py
+++ dict.py
@@ -1,1 +1,51 @@
 ###dictionaries
+##dictionary for Joy 
+joy_dict = {
+    'words': [
+        # Core Joy Words
+        'khush', 'khushi', 'maza', 'khoob', 'umda', 'aala', 'zabardast', 
+        'shaandaar', 'lovely', 'awesome', 'best', 'mubarak',
+        
+        # Extended Joy Vocabulary
+        'mast', 'josh', 'hansna', 'muskurana','dilchaspi',
+        'khushbu', 'roshan',
+        
+        # English Mix
+        'happy', 'joy', 'excited', 'fantastic', 'amazing', 'wonderful', 
+        'great', 'good', 'perfect', 'brilliant', 'excellent',
+
+      # Slang/Informal
+        'wah', 'wahwah', 'kya', 'jitna', 'bohot', 'bohat', 'zyada', 'zabardasti',
+        
+        # Additional Roman Urdu Joy Words
+        'khilkhilana', 'muskarahat', 'rang', 'roshni', 'chamak', 'jhoola',
+        'tarf', 'pasand', 'mohabbat', 'ishq', 'dil', 'jaan', 'prana',
+        'sukoon', 'araam', 'lazzat', 'swaad', 'mitha', 'meetha'
+    ],
+    
+}
+
+##dictionary for Sadness:
+sadness_dict = {
+    'words': [
+        # Core Sadness Words
+        'udaas', 'dukh', 'afsos', 'ranj', 'kharab', 'bura', 'tragedy', 
+        'miss', 'rona', 'ansu', 'dard', 'toota', 'tuta',
+        
+        # Extended Sadness Vocabulary
+        'dardnaak', 'peera', 'takleef', 'museebat', 'pareshan', 'bechain',
+        'gham', 'gum', 'mayusi', 'naummedi', 'shikwa', 'shikayat',
+        
+        # English Mix
+        'sad', 'unhappy', 'depressed', 'cry', 'tears', 'pain', 'hurt',
+        'broken', 'lost', 'bad', 'worst', 'awful', 'terrible',
+        
+        # Physical/Emotional States
+        'thak', 'thakan', 'bezaar', 'naaraaz', 'naraaz', 'gussa', 'narazgi',
+        
+        # Additional Roman Urdu Sadness Words
+        'veerana', 'suna', 'khaali', 'tanhai', 'akela', 'akelapan',
+        'andhera', 'ghabrahat', 'bezaari', 'nafrat', 'chubhan', 'zakham',
+        'dilgira', 'mout', 'intezar', 'judai', 'bichad', 'alvida'
+    ],
+}
