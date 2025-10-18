c258560e187ec6a24611c91e24c96319b4b01076
Zoha Fatima <zohafatima425@gmail.com>
2025-10-18 20:42:08 +0500

    Add anger and neutral emotion dictionaries

--- dict.py
+++ dict.py
@@ -48,3 +48,76 @@ sadness_dict = {
         'mout', 'intezar','bichad', 'alvida'
     ],
 }
+
+
+## Dictionary for Anger:
+anger_dict = {
+    'words': [
+        # Core Anger Words
+        'ghussa', 'gussa', 'naraz', 'naraaz', 'lanat', 'gusa', 
+        'gossah', 'khafa', 'chid', 'gazab', 'ghussah',
+        
+        # Intense Anger & Rage
+        'sakht_gussa', 'shadeed_gussa', 'beqaboo', 'aatish', 
+        'aatish_mizaj', 'aag', 'aag_bagola', 'deewana', 
+        'pagal', 'junoon',
+        
+        # Frustration & Annoyance
+        'tang', 'tang_aana', 'bezar', 'bezari', 'pareshan', 
+        'bechain', 'takleef', 'diqat',
+        
+        # Hate & Disgust
+        'nafrat', 'karahat', 'chir', 'chidchidahat', 'makkar', 
+        'nakara',
+        
+        # Insults & Cursing
+        'bewakoof', 'bayhooda', 'haywan', 'zalil', 'badtameez',
+        
+        # Betrayal & Injustice
+        'dhoka', 'bewafai', 'dagha', 'jaali', 'farebi', 'zulm', 'istimal',
+        
+        # Violence / Physical Anger
+        'maar', 'maar_peet', 'peet', 'peetna', 'kuchal', 'kuchalna', 
+        'tor', 'phar', 'tabah', 'barbaad',
+        
+        # Strong Metaphoric / Idiomatic Expressions
+        'zehr', 'pagalpan', 'sar_ghoom_gaya', 'dimaagh_kharab', 
+        'ulta_seedha_bolna', 'andaaz_dekh', 'dil_jal_gaya', 
+        'ghussay_mein_laal', 'chillana', 'dantna', 'phat_gaya', 
+        'tang_aa_gaya', 'barish_e_ghussa', 'dimagh_ka_dahi', 
+        'tooba_tooba', 'ulta_jawab', 'ghussa_chadhna', 
+        'jal_kar_raakh', 'phat_jaana', 'had_ho_gayi'
+    ],
+}
+
+
+## Dictionary for Neutral Emotion:
+neutral_dict = {
+    'words': [
+        # Basic Neutral Expressions
+        'theek', 'thik', 'ok', 'okay', 'fine',
+        
+        # Cognitive / Understanding
+        'samajh', 'samajh_gaya', 'koi_masla_nahi', 'maloom', 'pata', 
+        'samajhna', 'soch', 'sochna', 'dekha', 'suna', 'bata', 
+        'zahir', 'sach',
+        
+        # Descriptive / Ordinary
+        'aam', 'maamooli', 'mamuli', 'sada', 'seedha', 'mamla',
+        
+        # Routine Actions
+        'kaam', 'kar', 'karna', 'ja', 'jana', 'aa', 'aana',
+        'de', 'dena', 'le', 'lena', 'hogaya', 'hua', 'kiya',
+        
+        # Time and Place
+        'aaj', 'kal', 'ab', 'phir', 'yahan', 'wahan', 'idhar', 
+        'udhar', 'pehle', 'baad',
+        
+        # Quantities / Frequency
+        'kuch', 'thora', 'thoda', 'zyada', 'kum', 'aksar', 'kai',
+        
+        # Pronouns / References
+        'yeh', 'ye', 'woh', 'wo', 'koi', 'sab', 'har', 'har_aik', 'dono'
+    ],
+}
+
